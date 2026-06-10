bf3a4f1834d2384db6ede0c4006369095a05bea8
website-on <zyadromih123@gmail.com>
2026-06-10 21:56:27 +0300

    Add files via upload

--- generate_pages.js
+++ generate_pages.js
@@ -125,6 +125,10 @@ function getLayout(title, content) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>${title}</title>
+    <!-- Favicon (أيقونة الموقع) -->
+    <link rel="icon" type="image/png" href="images/favicon.png">
+    <link rel="shortcut icon" type="image/png" href="images/favicon.png">
+    <link rel="apple-touch-icon" href="images/favicon.png">
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;600;700;800;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="style.css">
